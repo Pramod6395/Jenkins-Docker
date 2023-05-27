f0dcebae2fad0215dd95c60b965406384c651ff7
Pramod6395 <pramod.patil@cimcondigital.com>
2023-05-27 20:03:34 +0530

    added htmls in app.py

--- app/app.py
+++ app/app.py
@@ -4,35 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Greetings</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f4f4f4;
-                text-align: center;
-                padding-top: 100px;
-            }
-
-            h1 {
-                color: #333333;
-            }
-
-            p {
-                color: #777777;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Hello ami!</h1>
-        <p>Hope you're doing great!</p>
-    </body>
-    </html>
-    '''
+    return 'Hello ami, hope your doing great!'
 
 if __name__ == '__main__':
     app.run()
+       
 
